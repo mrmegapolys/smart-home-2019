279d7ae6436a78231b58084a9c635dcdd3e2685f
Igor <rozhkovigor@yandex.ru>
2019-10-17 23:38:58 +0300

    initialization of processors tests

---
+++ src/main/test/ru/sbt/mipt/oop/eventprocessors/processors/SetDoorStateTest.java
@@ -0,0 +1,34 @@
+package ru.sbt.mipt.oop.eventprocessors.processors;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.sbt.mipt.oop.SmartHome;
+import ru.sbt.mipt.oop.SmartHomeProvider;
+import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
+
+import java.io.IOException;
+
+class SetDoorStateTest {
+    private static SmartHome smartHome;
+    private EventProcessor processor;
+
+    @BeforeAll
+    static void init() throws IOException {
+        String FILEPATH = "smart-home-1.js";
+        smartHome = SmartHomeProvider.readFile(FILEPATH, "json");
+    }
+
+    @BeforeEach
+    void setUp() {
+        processor = new SetDoorState(smartHome);
+    }
+
+    @Test
+    void process1() {
+    }
+
+    @Test
+    void process2() {
+    }
+}
