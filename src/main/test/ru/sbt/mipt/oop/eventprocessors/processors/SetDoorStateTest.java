53eb4eb63ae09a63eb34e6dfba91306e71b3a470
Igor <rozhkovigor@yandex.ru>
2019-10-19 15:55:41 +0300

    added unit tests

--- src/main/test/ru/sbt/mipt/oop/eventprocessors/processors/SetDoorStateTest.java
+++ src/main/test/ru/sbt/mipt/oop/eventprocessors/processors/SetDoorStateTest.java
@@ -2,11 +2,18 @@ package ru.sbt.mipt.oop.eventprocessors.processors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.sbt.mipt.oop.SensorEvent;
+import ru.sbt.mipt.oop.SensorEventType;
 import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
+import ru.sbt.mipt.oop.smarthome.Actionable;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 import ru.sbt.mipt.oop.smarthome.SmartHomeProvider;
+import ru.sbt.mipt.oop.smarthome.devices.door.Door;
+import ru.sbt.mipt.oop.smarthome.devices.door.DoorActionType;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SetDoorStateTest {
     private SmartHome smartHome;
@@ -19,10 +26,29 @@ class SetDoorStateTest {
     }
 
     @Test
-    void process1() {
+    void testClosedDoorOpens() {
+        String OBJECT_ID = "1";
+        SensorEvent event = new SensorEvent(SensorEventType.DOOR_EVENT, DoorActionType.OPEN, OBJECT_ID);
+        checkDoorCondition(false, OBJECT_ID);
+        processor.process(event);
+        checkDoorCondition(true, OBJECT_ID);
     }
 
     @Test
-    void process2() {
+    void testOpenDoorCloses() {
+        String OBJECT_ID = "3";
+        SensorEvent event = new SensorEvent(SensorEventType.DOOR_EVENT, DoorActionType.CLOSE, OBJECT_ID);
+        checkDoorCondition(true, OBJECT_ID);
+        processor.process(event);
+        checkDoorCondition(false, OBJECT_ID);
+    }
+
+    private void checkDoorCondition(boolean condition, String objectId) {
+        smartHome.execute( (Actionable actionable) -> {
+            if (!(actionable instanceof Door)) return;
+            Door door = (Door) actionable;
+            if (!(door.getId().equals(objectId))) return;
+            assertEquals(door.isOpen(), condition);
+        });
     }
 }
--- src/main/test/ru/sbt/mipt/oop/eventprocessors/processors/SetLightStateTest.java
+++ src/main/test/ru/sbt/mipt/oop/eventprocessors/processors/SetLightStateTest.java
@@ -2,11 +2,18 @@ package ru.sbt.mipt.oop.eventprocessors.processors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.sbt.mipt.oop.SensorEvent;
+import ru.sbt.mipt.oop.SensorEventType;
 import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
+import ru.sbt.mipt.oop.smarthome.Actionable;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 import ru.sbt.mipt.oop.smarthome.SmartHomeProvider;
+import ru.sbt.mipt.oop.smarthome.devices.light.Light;
+import ru.sbt.mipt.oop.smarthome.devices.light.LightActionType;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SetLightStateTest {
     private SmartHome smartHome;
@@ -19,10 +26,30 @@ class SetLightStateTest {
     }
 
     @Test
-    void process1() {
+    void testTurnedOffLightTurnsOn() {
+        String OBJECT_ID = "4";
+        SensorEvent event = new SensorEvent(SensorEventType.LIGHT_EVENT, LightActionType.ON, OBJECT_ID);
+        checkLightCondition(false, OBJECT_ID);
+        processor.process(event);
+        checkLightCondition(true, OBJECT_ID);
     }
 
     @Test
-    void process2() {
+    void testTurnedOnLightTurnsOff() {
+        String OBJECT_ID = "2";
+        SensorEvent event = new SensorEvent(SensorEventType.LIGHT_EVENT, LightActionType.OFF, OBJECT_ID);
+        checkLightCondition(true, OBJECT_ID);
+        processor.process(event);
+        checkLightCondition(false, OBJECT_ID);
     }
+
+    private void checkLightCondition(boolean condition, String objectId) {
+        smartHome.execute( (Actionable actionable) -> {
+            if (!(actionable instanceof Light)) return;
+            Light light = (Light) actionable;
+            if (!(light.getId().equals(objectId))) return;
+            assertEquals(light.isOn(), condition);
+        });
+    }
+
 }
