5e0da7090e212df63666efeebaff68a3e7ab8994
Igor <rozhkovigor@yandex.ru>
2019-10-28 13:37:58 +0300

    refactoring exception handling in smartHomeSerializer

--- src/main/java/ru/sbt/mipt/oop/smarthome/utils/SmartHomeSerializer.java
+++ src/main/java/ru/sbt/mipt/oop/smarthome/utils/SmartHomeSerializer.java
@@ -2,34 +2,23 @@ package ru.sbt.mipt.oop.smarthome.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 import ru.sbt.mipt.oop.smarthome.devices.alarm.Alarm;
 import ru.sbt.mipt.oop.smarthome.devices.alarm.AlarmState;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class SmartHomeSerializer<T> {
+
     public static SmartHome fromJson(String encoded) {
         Gson gson = getGson();
         SmartHome smartHome = gson.fromJson(encoded, SmartHome.class);
         setAlarmState(smartHome);
         return smartHome;
     }
-
-    private static void setAlarmState(SmartHome smartHome) { //TODO: refactor exception handling
-        Alarm alarm = smartHome.getAlarm();
-        AlarmState currentState = alarm.getState();
-        try {
-            AlarmState newState = currentState.getClass().getDeclaredConstructor(Alarm.class).newInstance(alarm);
-            Field stateField = alarm.getClass().getDeclaredField("state");
-            stateField.setAccessible(true);
-            stateField.set(alarm, newState);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     public static String toJson(SmartHome smartHome) {
         Gson gson = getGson();
@@ -39,7 +28,42 @@ public class SmartHomeSerializer<T> {
     private static Gson getGson() {
         return new GsonBuilder()
                 .registerTypeAdapter(AlarmState.class, new InterfaceAdapter<AlarmState>())
-                .setPrettyPrinting().
-                create();
+                .setPrettyPrinting()
+                .create();
     }
+
+    private static void setAlarmState(SmartHome smartHome) {
+        Alarm alarm = smartHome.getAlarm();
+        AlarmState currentState = alarm.getState();
+        AlarmState newState = getNewStateInstance(currentState, alarm);
+
+        try {
+            Field stateField = alarm.getClass().getDeclaredField("state");
+            stateField.setAccessible(true);
+            stateField.set(alarm, newState);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new JsonParseException(e);
+        }
+    }
+
+    private static AlarmState getNewStateInstance(AlarmState currentState, Alarm alarm) {
+        Constructor<? extends AlarmState> constructor;
+        try {
+            constructor = currentState.getClass().getDeclaredConstructor(Alarm.class);
+        } catch (NoSuchMethodException e) {
+            throw new JsonParseException(e);
+        }
+        constructor.setAccessible(true);
+
+        try {
+            return constructor.newInstance(alarm);
+        } catch (InstantiationException | InvocationTargetException e) {
+            throw new JsonParseException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
