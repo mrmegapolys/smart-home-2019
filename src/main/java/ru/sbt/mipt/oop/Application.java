55028fd09afcde0186cb47aa3ade057004d8aa47
Igor <rozhkovigor@yandex.ru>
2019-10-28 18:43:10 +0300

    decorators refactoring

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -4,12 +4,14 @@ import ru.sbt.mipt.oop.eventfactories.EventFactory;
 import ru.sbt.mipt.oop.eventfactories.RandomEventFactory;
 import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
 import ru.sbt.mipt.oop.eventprocessors.decorators.ActivatedAlarmDecorator;
+import ru.sbt.mipt.oop.eventprocessors.decorators.NotifierDecorator;
 import ru.sbt.mipt.oop.eventprocessors.processors.SetAlarmState;
 import ru.sbt.mipt.oop.eventprocessors.processors.SetDoorState;
 import ru.sbt.mipt.oop.eventprocessors.processors.SetLightState;
 import ru.sbt.mipt.oop.eventprocessors.processors.TurnLightsOffAfterClosingHallDoor;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 import ru.sbt.mipt.oop.smarthome.SmartHomeProvider;
+import ru.sbt.mipt.oop.smarthome.devices.alarm.Alarm;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -37,11 +39,23 @@ public class Application {
 
     private static List<EventProcessor> getProcessors(SmartHome smartHome) {
         Notifier notifier = new SMSNotifier();
+        Alarm alarm = smartHome.getAlarm();
         List<EventProcessor> processors = new ArrayList<>();
 
-        processors.add(new ActivatedAlarmDecorator(new SetDoorState(smartHome), smartHome, notifier));
-        processors.add(new ActivatedAlarmDecorator(new TurnLightsOffAfterClosingHallDoor(smartHome), smartHome, notifier));
-        processors.add(new ActivatedAlarmDecorator(new SetLightState(smartHome), smartHome, notifier));
+        EventProcessor setDoorState = new SetDoorState(smartHome);
+        setDoorState = new ActivatedAlarmDecorator(setDoorState, alarm);
+        setDoorState = new NotifierDecorator(setDoorState, alarm, notifier);
+        processors.add(setDoorState);
+
+        EventProcessor setLightState = new SetLightState(smartHome);
+        setLightState = new ActivatedAlarmDecorator(setLightState, alarm);
+        setLightState = new NotifierDecorator(setLightState, alarm, notifier);
+        processors.add(setLightState);
+
+        EventProcessor turnLightsOff = new TurnLightsOffAfterClosingHallDoor(smartHome);
+        turnLightsOff = new ActivatedAlarmDecorator(turnLightsOff, alarm);
+        processors.add(turnLightsOff);
+
         processors.add(new SetAlarmState(smartHome.getAlarm()));
 
         return processors;
--- src/main/java/ru/sbt/mipt/oop/eventprocessors/decorators/NotifierDecorator.java
+++ src/main/java/ru/sbt/mipt/oop/eventprocessors/decorators/NotifierDecorator.java
@@ -1,5 +1,6 @@
 package ru.sbt.mipt.oop.eventprocessors.decorators;
 
+import ru.sbt.mipt.oop.Notifier;
 import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
 import ru.sbt.mipt.oop.smarthome.devices.SensorEvent;
 import ru.sbt.mipt.oop.smarthome.devices.alarm.Activated;
@@ -7,12 +8,14 @@ import ru.sbt.mipt.oop.smarthome.devices.alarm.Alarm;
 import ru.sbt.mipt.oop.smarthome.devices.alarm.AlarmState;
 import ru.sbt.mipt.oop.smarthome.devices.alarm.Alert;
 
-public class ActivatedAlarmDecorator extends BaseProcessorDecorator {
+public class NotifierDecorator extends BaseProcessorDecorator {
     private final Alarm alarm;
+    private final Notifier notifier;
 
-    public ActivatedAlarmDecorator(EventProcessor delegate, Alarm alarm) {
+    public NotifierDecorator(EventProcessor delegate, Alarm alarm, Notifier notifier) {
         super(delegate);
         this.alarm = alarm;
+        this.notifier = notifier;
     }
 
     @Override
@@ -20,12 +23,7 @@ public class ActivatedAlarmDecorator extends BaseProcessorDecorator {
         if (!isSupportedEvent(event)) return;
         AlarmState state = alarm.getState();
 
-        if (state instanceof Alert) return;
-        if (state instanceof Activated) {
-            alarm.triggerAlert();
-            return;
-        }
+        if (state instanceof Activated || state instanceof Alert) notifier.sendNotification();
         super.process(event);
     }
-
 }
