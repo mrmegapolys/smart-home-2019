02cb3def392079eec5035c37530755a9a4cd88e1
Igor <rozhkovigor@yandex.ru>
2019-11-07 19:58:55 +0300

    migrated to Spring IoC (kind of)

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,52 +1,13 @@
 package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
-import ru.sbt.mipt.oop.eventprocessors.CCEventProcessorAdapter;
-import ru.sbt.mipt.oop.eventprocessors.EventProcessor;
-import ru.sbt.mipt.oop.eventprocessors.decorators.ActivatedAlarmDecorator;
-import ru.sbt.mipt.oop.eventprocessors.processors.*;
-import ru.sbt.mipt.oop.smarthome.SmartHome;
-import ru.sbt.mipt.oop.smarthome.SmartHomeProvider;
-import ru.sbt.mipt.oop.smarthome.devices.alarm.Alarm;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class Application {
-
     public static void main(String... args) {
-        String FILEPATH = "output.js";
-        SmartHome smartHome;
-
-        try {
-            smartHome = SmartHomeProvider.readFile(FILEPATH, "json");
-        } catch (IOException e) {
-            System.out.println("Failed to read smart home config.");
-            return;
-        }
-
-        List<EventProcessor> processors = getProcessors(smartHome);
-        SensorEventsManager manager = new SensorEventsManager();
-
-        for (EventProcessor processor : processors) {
-            manager.registerEventHandler(new CCEventProcessorAdapter(processor));
-        }
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        SensorEventsManager manager = context.getBean(SensorEventsManager.class);
         manager.start();
     }
-
-    private static List<EventProcessor> getProcessors(SmartHome smartHome) {
-        Notifier notifier = new SMSNotifier();
-        Alarm alarm = smartHome.getAlarm();
-        List<EventProcessor> processors = new ArrayList<>();
-
-        processors.add(new ActivatedAlarmDecorator(new DoorStateEventProcessor(smartHome), alarm));
-        processors.add(new ActivatedAlarmDecorator(new LightStateEventProcessor(smartHome), alarm));
-        processors.add(new ActivatedAlarmDecorator(new HallDoorEventProcessor(smartHome), alarm));
-        processors.add(new AlarmStateEventProcessor(alarm));
-        processors.add(new NotificationEventProcessor(alarm, notifier));
-
-        return processors;
-    }
-
 }
