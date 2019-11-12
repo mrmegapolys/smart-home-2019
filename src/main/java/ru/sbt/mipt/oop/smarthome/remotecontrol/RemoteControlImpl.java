197cc9fa12f4b0b13a279c437ab17fff9aa568d9
Igor <rozhkovigor@yandex.ru>
2019-11-12 13:14:11 +0300

    added remote control

---
+++ src/main/java/ru/sbt/mipt/oop/smarthome/remotecontrol/RemoteControlImpl.java
@@ -0,0 +1,31 @@
+package ru.sbt.mipt.oop.smarthome.remotecontrol;
+
+import rc.RemoteControl;
+import ru.sbt.mipt.oop.smarthome.remotecontrol.commands.Command;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class RemoteControlImpl implements RemoteControl {
+    private final Set<String> buttons;
+    private final Map<String, Command> commands = new HashMap<>();
+
+    public RemoteControlImpl(Set<String> buttons) {
+        this.buttons = buttons;
+    }
+
+    public boolean setCommand(String button, Command command) {
+        if (!buttons.contains(button)) return false;
+        commands.put(button, command);
+        return true;
+    }
+
+    @Override
+    public void onButtonPressed(String buttonCode, String rcId) {
+        if (!buttons.contains(buttonCode)) return;
+        Command command = commands.get(buttonCode);
+        if (command == null) return;
+        command.execute();
+    }
+}
